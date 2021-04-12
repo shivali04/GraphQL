31a6b514e78bd1549b1a6f2fb72f10583301a180
Shivali Singhal <shivali4.singhal@gmail.com>
2021-04-11 23:34:33 -0700

    Add files via upload

---
+++ Orders/Schema/OrdersSchemas.cs
@@ -0,0 +1,20 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Orders.Schema
+{
+    public class OrdersSchema : GraphQL.Types.Schema
+    {
+        public OrdersSchema(OrdersQuery query, IDependencyResolver resolver)
+        {
+            Query = query;
+            DependencyResolver = resolver;
+        }
+
+        public IDependencyResolver DependencyResolver { get; }
+    }
+}
---
+++ Orders/Services/CustomerService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orders.Models;
+
+namespace Orders.Services
+{
+  public  class CustomerService : ICustomerService
+    {
+        private List<Customer> _customers;
+
+        public CustomerService()
+        {
+            _customers.Add(new Customer(1, "KinetEco"));
+            _customers.Add(new Customer(2, "Pixelford Photography"));
+            _customers.Add(new Customer(3, "Topsy Turvy"));
+            _customers.Add(new Customer(4, "Leaf & Mortar"));
+        }
+        public Customer GetCoustomerById(int id)
+        {
+            return GetCustomerByIdAsync(id).Result;
+        }
+
+        public Task<Customer> GetCustomerByIdAsync(int id)
+        {
+            return Task.FromResult(_customers.Single(o => Equals(o.Id, id)));
+        }
+
+        public Task<IEnumerable<Customer>> GetCustomersAsync()
+        {
+            return Task.FromResult(_customers.AsEnumerable());
+        }
+    }
+    public interface ICustomerService
+    {
+        Customer GetCoustomerById(int id);
+        Task<Customer> GetCustomerByIdAsync(int id); //allows to return tasks, for scalable, responsive APIs
+        Task<IEnumerable<Customer>> GetCustomersAsync();
+    }
+}
