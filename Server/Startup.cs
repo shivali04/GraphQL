eb4f9c8d9f57b888f69065bd1a12df916a72aba1
Shivali Singhal <shivali4.singhal@gmail.com>
2021-04-11 23:35:29 -0700

    Server files

---
+++ Server/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Orders.Schema;
+using Orders.Services;
+namespace Server
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+             services.AddSingleton<IOrderService, OrderService>();
+            services.AddSingleton<ICustomerService,CustomerService>();
+            services.AddSingleton<OrderType>();
+            services.AddSingleton<CustomerType>();
+            services.AddSingleton<OrderStatusesEnum>();
+            services.AddSingleton<OrdersQuery>();
+            services.AddSingleton<OrdersSchemas>();
+            //also depdencyresolver all would be available to graphl
+            services.AddSingleton<IDependencyResolver>(c=> new FuncDependencyResolver(Type=> c.GetRequiredService(type)));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+
+    internal interface IDependencyResolver
+    {
+    }
+}
